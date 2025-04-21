ab257d5dc2184bcf9b6f3b8d310bdd55a4a63809
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-04-21 00:53:50 -0400

    mods

--- Browser-Extension-Design/src/App.jsx
+++ Browser-Extension-Design/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import Header from './components/Header.jsx'
 
 function App() {
 
   return (
     <div className="app-container">
+      <Header />
 
     </div>
-  )
+  );
 }
 
 export default App
---
+++ Browser-Extension-Design/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import './styles/Header.css'
+import logoS from './assets/images/logo.svg'
+import sun from './assets/images/icon-sun.svg'
+
+function Header() {
+    <div className="heady">
+        <div className="headL">
+            <img src={logoS} alt="logo" />
+            <h3>Extensions</h3>
+        </div>
+        <div className="toggler">
+            <img src={sun} alt="Sun Symbol" />
+        </div>
+    </div>
+
+}
+
+
+export default Header;
