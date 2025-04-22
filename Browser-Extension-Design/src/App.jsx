96dae0c55c0acdcdcc97f04fbc5c1039d467e254
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-04-22 00:26:48 -0400

    new component

--- Browser-Extension-Design/src/App.jsx
+++ Browser-Extension-Design/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header.jsx'
+import FilterBar from './components/FilterBar.jsx'
 
 function App() {
 
   return (
     <div className="app-container">
       <Header />
+      <FilterBar />
       
 
     </div>
---
+++ Browser-Extension-Design/src/components/FilterBar.jsx
@@ -0,0 +1,18 @@
+import '../styles/FilterBar.css'
+
+
+function FilterBar() {
+    return (
+        <div className="filter-container">
+            <h2>Extensions List</h2>
+            <div className="btn-container">
+                <button className="all">All</button>
+                <button className="active">Active</button>
+                <button className="inactive">Inactive</button>
+        </div>
+    </div>
+    )
+    
+}
+
+export default FilterBar
