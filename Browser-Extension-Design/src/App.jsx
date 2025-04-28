4ac6031569197295c00adbb6876afb5e25c8ceca
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-04-28 16:33:09 -0400

    new mods

--- Browser-Extension-Design/src/App.jsx
+++ Browser-Extension-Design/src/App.jsx
@@ -1,13 +1,36 @@
 import './App.css'
 import Header from './components/Header.jsx'
 import FilterBar from './components/FilterBar.jsx'
+import Card from './components/Card.jsx'
+
+// All images here:
+import devlens from './assets/images/logo-devlens.svg'
 
 function App() {
+
+  const cardData = [
+    {
+      imgSrc: devlens,
+      title: "Devlens",
+      desc: "Quickly inspect page layouts and visualize element boundaries."
+    }
+  ]
 
   return (
     <div className="app-container">
       <Header />
       <FilterBar />
+      <div className="cardList">
+        {cardData.map((card, index) => (
+          <Card 
+          key={index}
+          imgSrc={card.imgSrc}
+          title={card.title}
+          desc={card.desc}
+          />
+        ))}
+
+      </div>
     </div>
   );
 }
--- Browser-Extension-Design/src/components/Card.jsx
+++ Browser-Extension-Design/src/components/Card.jsx
@@ -1,5 +1,19 @@
-function Card() {
-
+function Card({imgSrc, title, desc}) {
+    return (
+        <div className="card-container">
+            <section className="top">
+                <img src={imgSrc} alt={title} />
+                    <div className="header-section">
+                        <h3>{title}</h3>
+                        <p>{desc}</p>
+                    </div>
+            </section>
+            <div className="bottom">
+                <button className="removeBtn">Remove</button>
+                <input type="checkbox" className="checker" />
+            </div>
+        </div>
+    );
 }
 
 export default Card
