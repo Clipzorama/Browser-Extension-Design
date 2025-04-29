a329b4e4cafd8ab10dbe37405308ccf9060470ee
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-04-29 15:26:24 -0400

    updated logic for the filterbar

--- Browser-Extension-Design/src/components/FilterBar.jsx
+++ Browser-Extension-Design/src/components/FilterBar.jsx
@@ -6,8 +6,8 @@ function FilterBar() {
         <div className="filter-container">
             <h2>Extensions List</h2>
             <div className="btn-container">
-                <button className="all">All</button>
-                <button className="active">Active</button>
+                <button className="active">All</button>
+                <button className="inactive">Active</button>
                 <button className="inactive">Inactive</button>
             </div>
         </div>
