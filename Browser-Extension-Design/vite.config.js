e71b94eb8807bfb7a4608f89026f6c9fd890da6e
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-05-05 19:47:55 -0400

    enabling gh-pages

--- Browser-Extension-Design/vite.config.js
+++ Browser-Extension-Design/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Browser-Extension-Design/", // for gh-pages, this should match the repository name
   plugins: [react()],
 })
